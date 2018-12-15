38e50e407f3ba36d61dc69c87b5231961b13b9fa
nathi <nathi.t@outlook.com>
2018-12-15 23:27:04 +0000

    add register controller and request

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use App\User;
+
+class RegisterController extends Controller
+{
+    public function register(StoreUserRequest $request)
+    {
+
+        $user = new User;
+        $user->username = $request->username;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+
+        $user->save();
+    }
+}
